b70d803713bf8e8858c86f06b8fca3e5bd71502b
johnson-abraham <johnsonabraham0812@gmail.com>
2025-11-11 18:45:55 +0530

    other files

---
+++ query/total-revenue-query-options.ts
@@ -0,0 +1,7 @@
+import { queryOptions } from "@tanstack/react-query";
+import { fetchTotalRevenue } from "@/db/kpi";
+
+export const totalRevenueQueryOptions = queryOptions({
+  queryKey: ["kpi", "totalRevenue"],
+  queryFn: fetchTotalRevenue,
+});
